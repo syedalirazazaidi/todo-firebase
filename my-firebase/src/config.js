a8cfec5ec28aa2535ce17011eef7fee9b835e3f3
ali raza <aaliraza007@hotmail.com>
2019-07-29 00:14:28 +0500

    index.html

---
+++ my-firebase/src/config.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ //const firebaseConfig = {
+    const config ={
+    apiKey: "AIzaSyA55w4xscf-WtWykirNyMKyHtnk6EhmSYI",
+    authDomain: "react-app-2a5ba.firebaseapp.com",
+    databaseURL: "https://react-app-2a5ba.firebaseio.com",
+    
+  };
+  // Initialize Firebase
+  //firebase.initializeApp(firebaseConfig);
+ // export default firebaseConfig;
+ export default config;
